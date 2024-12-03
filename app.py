1c38dbe1d7564e0890c2dccca9749c2766bec6f4
Kenji Sato <mail@kenjisato.jp>
2024-12-03 10:26:21 +0900

    update material

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from shiny.express import input, render, ui
+from shinyswatch import theme
+
+ui.page_opts(theme=theme.minty)
+
+ui.tags.link(href="css/style.css", rel="stylesheet")
+
+ui.h1("My app")
 
 ui.input_slider("val", "Slider label", min=0, max=100, value=50)
 
 @render.text
 def slider_val():
     return f"Slider value: {input.val()}"
+
+ui.div(
+    ui.h2("こんにちは"),
+    ui.p("Ciao"),
+    class_="hello"
+)
